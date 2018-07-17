f81266cf720be2b26e5a27a46635284627b40d2e
szakon <sarahzakon@gmail.com>
2018-07-17 18:03:29 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,10 @@ class ApplicationController < Sinatra::Base
   get "/" do
   	erb :index
   end
+  get '/bio' do 
+    erb :karliebio
+  end
+  get '/karliekubs' do 
+    erb :karlieskubs
+  end
 end
